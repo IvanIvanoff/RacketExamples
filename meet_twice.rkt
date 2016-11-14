3cbefd2a33c5601eea2e89a713d22b7ab4b33d53
Ivan Ivanoff <IvanIvanoff@users.noreply.github.com>
2016-11-14 17:22:25 +0200

    Add new solution and comments

--- meet_twice.rkt
+++ meet_twice.rkt
@@ -3,6 +3,7 @@
 ; която проверява дали в целочисления интервал [a; b] съществуват две
 ; различни цели числа x и y такива, че f(x) = g(x) и f(y) = g(y).
 
+; Решение 1: С използване на функции, които могат да се използват в повече от една задача веднъж дефинирани
 (define (from-to a b)
   (if (= a b) (cons a '())
       (cons a (from-to (+ a 1) b))))
@@ -19,3 +20,15 @@
   (define lst1 (map f lst))
   (define lst2 (map g lst))
   (>= (length (same-at-same-position lst1 lst2)) 2))
+  
+  ; Решение 2
+  
+  (define (meetTwice2? f g a b)
+    (define (mt? x counter)
+      (cond[(and (> x b) (< counter 2)) #f]
+           [(>= counter 2) #t]
+           [(= (f x) (g x)) (mt? (+ 1 x) (+ 1 counter))]
+           [else (mt? (+ 1 x) counter)]))
+
+    (mt? a 0)
+)
