71f53de4075826524eb064379d18e39564b085c5
Ivan Ivanoff <IvanIvanoff@users.noreply.github.com>
2016-11-15 10:30:03 +0200

    Add second solution

--- longest_sublist.rkt
+++ longest_sublist.rkt
@@ -3,6 +3,8 @@
 ; на списъка от числа l с максимална дължина. Ако съществуват няколко такива подсписъка,
 ; функцията да върне първия отляво надясно.
 
+; Решение 1
+
 (define get-n-items
     (lambda (lst num)
         (if (> num 0)
@@ -30,3 +32,25 @@
 (longestDescending­ '(5 3 8 6 4 2 6 7 1))
 
 (longestDescending­ '(1 2 3 4 5 6))
+
+; Решение 2 - shorter
+
+
+(define (take-from-to lst a b)
+  (if (> a b) '()
+      (drop (take lst b) a)))
+
+(define (maxAsc lst)
+  (define (ma l curr pos len max_pos max_len)
+    (cond [(or (null? l) (null? (cdr l)))
+               (take-from-to lst max_pos (+ max_pos max_len))]
+               ; (cons max_pos max_len)]
+          [(< (car l) (car (cdr l)))
+               (if (> (+ 1 len) max_len)
+                   (ma (cdr l) (+ 1 curr) pos (+ 1 len) pos (+ 1 len))
+                   (ma (cdr l) (+ 1 curr) pos (+ 1 len) max_pos max_len))]
+          [else (ma (cdr l) (+ 1 curr) (+ 1 curr) 1 max_pos max_len)]))
+
+  (ma lst 0 0 1 1 1))
+
+(maxAsc '(1 2 3 1 4 5))
